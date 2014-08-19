d3d4a184e53c45b74a7587a909c2433555632eae
pigram86 <todd@toddpigram.com>
2014-08-19 07:24:40 -0400

    rename

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name             'win_srv_default'
 maintainer       'Todd Pigram'
 maintainer_email 'todd@toddpigram.com'
 license          'Apache 2.0'
-description      'Installs/Configures windows_server_default'
+description      'Installs/Configures windows_srv_default'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.5.0'
 depends          'windows'
